20de5e46064fa77aca4b424541449bb04e0e8108
vishu22 <vishal.tandon568@gmail.com>
2015-04-15 14:10:04 +0530

    Update test_autopep.py

--- test/test_autopep.py
+++ test/test_autopep.py
@@ -6,8 +6,7 @@ import os
 class Testing(unittest.TestCase):
     def test_pepexample(self):
         #Converting code into pep8 style
-        path = os.getcwd() + '\\autopep_test.py'
-        subprocess.check_output('autopep8 --in-place --aggressive --aggressive ' + path)
+        subprocess.check_output('autopep8 --in-place --aggressive --aggressive autopep_test.py')
         try:
             #checking the code against pep8
             subprocess.check_output('pep8 --first autopep_test.py')
